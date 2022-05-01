494b84d912b153ed224a562ac8dbeae52d508da3
Haseeba Fathiya <58848482+haseefathi@users.noreply.github.com>
2022-04-30 20:19:11 -0400

    Update alert.py

--- codes/alert.py
+++ codes/alert.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 import datetime
 
-account_sid = 'AC7794fc5f48e1453642a2e7e5628c8086'
-auth_token = '413044b6a8c546d9ca77d074bfb4e73b'
+account_sid = '******************'
+auth_token = '*********************'
 client = Client(account_sid, auth_token)
 
 
@@ -10,7 +10,7 @@ def send_alert():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     message = client.messages.create(
-        from_='whatsapp:+14155238886',
+        from_='whatsapp:+***********',
         body=f'No Face Mask Alert - Camera 0 at {current_time}',
-        to='whatsapp:+16462919529'
+        to='whatsapp:+***********'
     )
